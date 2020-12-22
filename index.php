38a654bb088b6b4660f21a9f3c3fc0ad119a1fc9
meshikhah <masihsaeedi.s@gmail.com>
2020-12-22 19:57:51 +0330

    database initial

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+require_once "database.php";
+abstract class person
+{
+    public $name;
+    public $family;
+
+    function getName()
+    {
+        return $this->name;
+    }
+
+    function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    function getFamily()
+    {
+        return $this->family;
+    }
+
+    function setFamily($family)
+    {
+        $this->family = $family;
+    }
+}
+
+
+
+
+
+
+
+?>
