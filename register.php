8cd5461c19617f2e8cfe5388e0780c4d2c1a9d7b
meshikhah <masihsaeedi.s@gmail.com>
2020-12-22 20:22:12 +0330

    register added

---
+++ register.php
@@ -0,0 +1,57 @@
+<?php
+require "config.php";
+require "model/user.php";
+include $ShareFolderPath."header.html";
+
+$Message = '';
+$uiName_cv = "";
+$uiFamily_cv = "";
+$uiUsername_cv = "";
+
+if(isset($_POST['uiRegister']))
+{
+    $uiName_cv = $_POST['uiName'];
+    $uiFamily_cv = $_POST['uiFamily'];
+    $uiUsername_cv = $_POST['uiUsername'];
+
+    $validationMessage = validation();
+    if($validationMessage == "") {
+        $u = new user();
+        $u->setName($_POST['uiName']);
+        $u->setFamily($_POST['uiFamily']);
+        $u->setUsername($_POST['uiUsername']);
+        $u->setPassword($_POST['uiPassword']);
+        if($u->Save())
+            $Message = 'You have successfully registed.';
+        else
+            $Message = 'The username already exists. Please use a different username.';
+    }
+    else
+        $Message = $validationMessage;
+}
+
+
+include $ViewPath."register.html";
+
+include $ShareFolderPath."footer.html";
+
+
+function validation()
+{
+    $Message = "";
+    if($_POST["uiName"] == "")
+        $Message = 'Enter your name'."<br/>";
+    if($_POST["uiFamily"] == "")
+        $Message .= 'Enter your family'."<br/>";
+    if($_POST["uiUsername"] == "")
+        $Message .= 'Enter your username.'."<br/>";
+    if($_POST["uiPassword"] == "")
+        $Message .= 'Enter your password'."<br/>";
+
+    if($_POST["uiPassword"] != $_POST["uiConfirmPassword"])
+        $Message .= 'Password and confirmation password do not match.'."<br/>";
+
+    return $Message;
+}
+
+?>
