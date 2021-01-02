f65a296e1b531fbc648a5bffab298db201bf57d4
ffallah7575 <74931885+ffallah7575@users.noreply.github.com>
2021-01-03 00:53:06 +0330

    Add files via upload

---
+++ Login.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+unset($_REQUEST['USER']);
+require "model/user.php";
+include "index.html";
+$Message = '';
+if(isset($_POST['uiLogin']))
+{
+    $user = new user();
+    $user->set_username($_POST['uiUsername']);
+    $user->set_password($_POST['uiPassword']);
+
+    if($user->Check_User_Pass())
+    {
+        $_SESSION['USER'] = serialize($Uuser);
+        header('Location:chat_room.php');
+    }
+
+    $Message = 'Invalid username or password.';
+}
+
+?>
---
+++ Register.php
@@ -0,0 +1,64 @@
+<?php
+require "config.php";
+require "modeluser.php";
+include "index.html";
+
+
+$Message = '';
+$Username = "";
+$Password="";
+$Phone_Number = "";
+
+
+if(isset($_POST['Register']))
+{
+    $Username = $_POST['UserName'];
+    $Password = $_POST['Password'];
+    $Phone_Number = $_POST['PhoneNumber'];
+
+    $Validation_Message = validation();
+    if($Validation_Message == "")
+    {
+        $user = new user();
+        $user->set_username($_POST['UserName']);
+        $user->set_password($_POST['Password']);
+        $user->set_Phone_Number($_POST['PhoneNumber']);
+        if($user->Save())
+        {
+            $Message = 'You have successfully Registed.';
+            header('Location: Message.php');
+        }
+        else
+        {
+            $Message =  'The username already exists. Please use a different.';
+        }
+        else
+        {
+            $Message = $Validation_Message;
+        }
+    }
+}
+
+function validation()
+{
+    $Message = "";
+    if($_POST["Username"]=="")
+    {
+        $Message = 'Enter Your name.'."<br/>";
+    }
+    if($_POST["Password"] == "")
+    {
+        $Message = 'Enter your family.'."<br/>";
+    }
+    if($_POST["Conf_Pass"] == "")
+    {
+        $Message = 'Enter Confirm Password';
+    }
+    if($_POST["Password"] != $_POST["Conf_Pass"])
+    {
+        $Message .= 'Password and confirmation password not match.'."<br/>";
+    }
+
+    return $Message ;
+}
+?>
