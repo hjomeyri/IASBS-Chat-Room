f90760ab03fb0df7d373c5f7d8fc2ee568d099ec
hjomeyri <47713382+hjomeyri@users.noreply.github.com>
2021-01-04 18:40:40 +0330

    Update config.php

--- config.php
+++ config.php
@@ -1,4 +1,9 @@
 <?php
+$server = 'localhost';
+$db_username = 'root';
+$db_password = '1234';
+$db_name = 'iasbschatroom';
+
 $ViewPath = 'view/';
 $SharedViewPath = 'view/shared/';
 $ModelPath = 'model/'
